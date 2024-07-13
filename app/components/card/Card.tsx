36bc1e02459cb56b871b474f166d8f873619122e
EEstaniel <edwineestaniel93@gmail.com>
2024-07-13 16:16:39 -0700

    feat: add animations to shopping cart

--- app/components/card/Card.tsx
+++ app/components/card/Card.tsx
@@ -1,30 +1,33 @@
 import styles from "./Card.module.css";
-import { getImageSize } from "~/utils/getImageSize";
-import { CartButton } from "~/components/buttons/cart_button/CartButton";
+import {getImageSize} from "~/utils/getImageSize";
+import {CartButton} from "~/components/buttons/cart_button/CartButton";
 import globals from "~/styles/globals.module.css";
-import { useIsProductInCart } from "~/store/ProductStore";
-import { motion } from "framer-motion";
+import {useIsProductInCart} from "~/store/ProductStore";
+import {motion} from "framer-motion";
+
 
 interface CardProps {
   product: any;
 }
 
-export const Card = ({ product }: CardProps) => {
+
+export const Card = ({product}: CardProps) => {
   const isActive = useIsProductInCart();
 
   return (
     <>
       <motion.div className={styles.img_button_group}
-      whileHover={{ scale: 1.02 }}
-      >
+                  whileHover={{scale: 1.02}}>
+
         <img
+
           className={`${isActive(product.name) && styles.active}`}
           src={product.image[getImageSize() as any]}
           alt={product.name}
           loading="lazy"
         />
         <div className={styles.button_container}>
-          <CartButton product_name={product.name} />
+          <CartButton product_name={product.name}/>
         </div>
       </motion.div>
       <div className={styles.item_info_group}>
--- app/components/product_list/ProductList.tsx
+++ app/components/product_list/ProductList.tsx
@@ -1,34 +1,36 @@
 import styles from "./ProductList.module.css";
 import globals from "../../styles/globals.module.css";
-import { Card } from "~/components/card/Card";
-import { useProductData } from "~/store/ProductStore";
-import { motion } from "framer-motion";
-
+import {Card} from "~/components/card/Card";
+import {useProductData} from "~/store/ProductStore";
+import {AnimatePresence, motion} from "framer-motion";
 
 
 export const ProductList = () => {
   const productData = useProductData();
 
   return (
-    <div className={styles.main_content}>
+
+    <motion.div className={styles.main_content}>
       <div className={styles.product_list}>
         <h1 className={globals.text_preset_1}>Desserts</h1>
         <ul
           className={styles.list_container}
         >
           {Object.keys(productData).map((name, i) => (
-            <motion.li
-              key={name}
-              className={`${globals.text_preset_1} ${styles.list_item}`}
-              initial={{ x: 100, opacity: 0 }}
-              animate={{ x: 0, opacity: 1, transition: { delay: i * 0.07 } }}
+            <AnimatePresence key={i}>
+              <motion.li
+                key={name}
+                className={`${globals.text_preset_1} ${styles.list_item}`}
+                initial={{x: 100, opacity: 0}}
+                animate={{x: 0, opacity: 1, transition: {delay: i * 0.07}}}
 
-            >
-              <Card product={productData[name]} />
-            </motion.li>
+              >
+                <Card product={productData[name]}/>
+              </motion.li>
+            </AnimatePresence>
           ))}
         </ul>
       </div>
-    </div>
+    </motion.div>
   );
 };
