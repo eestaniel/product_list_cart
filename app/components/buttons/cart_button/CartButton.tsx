37f2f8554cc58591109ad1dd7f52ae6cb4bf73a6
EEstaniel <edwineestaniel93@gmail.com>
2024-07-13 17:38:22 -0700

    feat: add animation button

--- app/components/buttons/cart_button/CartButton.tsx
+++ app/components/buttons/cart_button/CartButton.tsx
@@ -1,24 +1,20 @@
-import {useCallback, useRef} from "react";
+import { useCallback, useRef } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import styles from "./CartButton.module.css";
 import globals from "~/styles/globals.module.css";
-import {AddIcon, IconAddToCart, SubtractIcon} from "~/components/icons/Icons";
+import { AddIcon, IconAddToCart, SubtractIcon } from "~/components/icons/Icons";
 import {
   useAddProduct,
   useIsProductInCart,
   useProductCart,
-  useRemoveProduct
+  useRemoveProduct,
 } from "~/store/ProductStore";
-
 
 interface CartButtonProps {
   product_name: string;
 }
 
-
-export const CartButton = ({
-                             product_name,
-                           }: CartButtonProps) => {
-
+export const CartButton = ({ product_name }: CartButtonProps) => {
   /* -------Import States------- */
   const cartButtonRef = useRef(null);
   const addProduct = useAddProduct();
@@ -26,57 +22,74 @@ export const CartButton = ({
   const ProductCart = useProductCart();
   const isActive = useIsProductInCart();
 
-  const handleAddToCart = useCallback((event: {
-    stopPropagation: () => void;
-  }) => {
-    event.stopPropagation();
-    addProduct(product_name)
-  }, [product_name, addProduct]);
+  const handleAddToCart = useCallback(
+    (event: { stopPropagation: () => void }) => {
+      event.stopPropagation();
+      addProduct(product_name);
+    },
+    [product_name, addProduct]
+  );
 
-  const handleIncrement = useCallback((event: {
-    stopPropagation: () => void;
-  }) => {
+  const handleIncrement = useCallback(
+    (event: { stopPropagation: () => void }) => {
+      event.stopPropagation();
+      addProduct(product_name);
+    },
+    [product_name, addProduct]
+  );
 
-    event.stopPropagation();
-    addProduct(product_name)
-  }, [product_name, addProduct]);
-
-  const handleDecrement = useCallback((event: {
-    stopPropagation: () => void;
-  }) => {
-    event.stopPropagation();
-    removeProduct(product_name);
-  }, [product_name, removeProduct]);
+  const handleDecrement = useCallback(
+    (event: { stopPropagation: () => void }) => {
+      event.stopPropagation();
+      removeProduct(product_name);
+    },
+    [product_name, removeProduct]
+  );
 
   return (
     <div ref={cartButtonRef}>
-      <div
-        className={`
-        ${styles.cart_button_container} 
-        ${globals.text_preset_4}
-        ${isActive(product_name) && styles.active_button}
-        `}
-        onClick={!isActive(product_name) ? handleAddToCart : undefined}
-      >
+      <AnimatePresence mode={'wait'}>
         {!isActive(product_name) ? (
-          <>
+          <motion.div
+            key="add-to-cart"
+            className={`
+              ${styles.cart_button_container} 
+              ${globals.text_preset_4}
+            `}
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1, transition: { duration: 0.05 }}}
+            exit={{ opacity: 0, scale: 0.8, transition: { duration: 0.05 }}}
+            onClick={handleAddToCart}
+          >
             <div className={styles.svg_container}>
-              <IconAddToCart/>
+              <IconAddToCart />
             </div>
             <span className={globals.text_preset_4_bold}>Add to Cart</span>
-          </>
+          </motion.div>
         ) : (
-          <>
+          <motion.div
+            key="increment-decrement"
+            className={`
+              ${styles.cart_button_container} 
+              ${globals.text_preset_4}
+              ${styles.active_button}
+            `}
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1, transition: { duration: 0.05 }}}
+            exit={{ opacity: 0, scale: 0.8, transition: { duration: 0.05 }}}
+          >
             <div onClick={handleDecrement}>
-              <SubtractIcon/>
+              <SubtractIcon />
             </div>
-            <span className={globals.text_preset_4_bold}>{ProductCart[product_name].quantity}</span>
+            <span className={globals.text_preset_4_bold}>
+              {ProductCart[product_name].quantity}
+            </span>
             <div onClick={handleIncrement}>
-              <AddIcon/>
+              <AddIcon />
             </div>
-          </>
+          </motion.div>
         )}
-      </div>
+      </AnimatePresence>
     </div>
   );
 };
